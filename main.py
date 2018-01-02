7050878410e2a53ec54ea63f54fdc0e2a0a75fc2
yy-bird <guoqiang2648@gmail.com>
2018-01-02 09:17:04 +0800

    adjust parameter

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def check_price(productId, lowerLimit):
     info['text'] = '{0} - {1} buy: {2}, sell: {3}'.format(datetime.datetime.now().strftime("%I:%M%p"), data['currency_pair_code'], data['market_bid'], data['market_ask'])
     print(info)
     
-    if data['market_ask'] <= lowerLimit:
+    if data['market_bid'] >= lowerLimit:
         requests.post("https://hooks.slack.com/services/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", data={"payload": json.dumps(info)})
 
     
 
 def scheduler(s):
-   s.enter(5, 1, check_price, argument=('57', 0.86,))
-   s.enter(5, 1, check_price, argument=('1', 14600,))
+   s.enter(5, 1, check_price, argument=('57', 0.9,))
+   s.enter(5, 1, check_price, argument=('1', 15000,))
    s.run()
 
 while(1):
