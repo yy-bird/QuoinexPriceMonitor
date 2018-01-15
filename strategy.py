792d50f85718ebd0478498d29ad73a8e3e0ac72d
yy-bird <guoqiang2648@gmail.com>
2018-01-16 02:44:03 +0800

    add checking

--- strategy.py
+++ strategy.py
@@ -6,7 +6,6 @@ import json, requests, math
 class Strategy:
     client = Quoinex(Account())
     products_info = {}
-    base_fund = 100
 
     def run(self, patterns):
         self._get_products_info()
@@ -18,24 +17,27 @@ class Strategy:
         from_product_ask = self.products_info[pattern["from_product"].value]["ask"]
         mid_product_bid = self.products_info[pattern["mid_product"].value]["bid"]
         to_product_bid = self.products_info[pattern["to_product"].value]["bid"]
-        print('{0} positive: {1}'.format(pattern["mid_product"].name, 100/from_product_ask*mid_product_bid*to_product_bid))
+        ratio = 100/from_product_ask*mid_product_bid*to_product_bid        
+        print('{0} positive: {1}'.format(pattern["mid_product"].name, ratio))
 
-        if 100/from_product_ask*mid_product_bid*to_product_bid > 100.2:
+        if ratio > 100.2:
             amount = math.floor(pattern["base_fund"]/from_product_ask)
-            from_buy = self.client.buy(pattern["from_product"], amount)
-            mid_sell= self.client.sell(pattern["mid_product"], from_buy["quantity"])
-            to_sell = self.client.sell(pattern["to_product"], mid_sell["total"])
-            print('{0} positive: start: {1}, end: {2}, earning: {3}'.format(pattern["mid_product"].name, from_buy["total"], to_sell["total"], to_sell["total"]-from_buy["total"]))
-            info = {'text': '{0} positive earning: {1}'.format(pattern["mid_product"].name, round(to_sell["total"]-from_buy["total"], 3))}
-            requests.post("https://hooks.slack.com/services/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", data={"payload": json.dumps(info)})
+            if float(self.client.get_orderbook(pattern["from_product"])["sell_price_levels"][1]) > amount*0.75:
+                from_buy = self.client.buy(pattern["from_product"], amount)
+                mid_sell= self.client.sell(pattern["mid_product"], from_buy["quantity"])
+                to_sell = self.client.sell(pattern["to_product"], mid_sell["total"])
+                print('{0} positive: start: {1}, end: {2}, earning: {3}'.format(pattern["mid_product"].name, from_buy["total"], to_sell["total"], to_sell["total"]-from_buy["total"]))
+                info = {'text': '{0} positive earning: {1}'.format(pattern["mid_product"].name, round(to_sell["total"]-from_buy["total"], 3))}
+                requests.post("https://hooks.slack.com/services/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", data={"payload": json.dumps(info)})
 
     def _negative_trade(self, pattern):
         from_product_bid = self.products_info[pattern["from_product"].value]["bid"]
         mid_product_ask = self.products_info[pattern["mid_product"].value]["ask"]
         to_product_ask = self.products_info[pattern["to_product"].value]["ask"]
-        print('{0} negative: {1}'.format(pattern["mid_product"].name, 100/to_product_ask/mid_product_ask*from_product_bid))
+        ratio = 100/to_product_ask/mid_product_ask*from_product_bid
+        print('{0} negative: {1}'.format(pattern["mid_product"].name, ratio))
 
-        if 100/to_product_ask/mid_product_ask*from_product_bid > 100.2:
+        if ratio > 100.2:
             amount = self._process_price(pattern["base_fund"]/to_product_ask)
             to_buy = self.client.buy(pattern["to_product"], amount)
             mid_buy = self.client.buy(pattern["mid_product"], self._process_price(to_buy["quantity"]/mid_product_ask))
