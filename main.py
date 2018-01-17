8131b2b0cda3ac7dae3afbde70966dc4c13dc935
yy-bird <guoqiang2648@gmail.com>
2018-01-18 01:38:08 +0800

    minor adjustment

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ trade_info = {}
 
 def scheduler_trade(s):
     product_trade = [{"from_product": Product.QASHUSD, "mid_product": Product.QASHBTC, "to_product": Product.BTCUSD, "base_fund": 50}]
-    product_trade.append({"from_product": Product.QASHUSD, "mid_product": Product.QASHETH, "to_product": Product.ETHUSD, "base_fund": 50})
-    product_trade.append({"from_product": Product.ETHUSD, "mid_product": Product.ETHBTC, "to_product": Product.BTCUSD, "base_fund": 50})
+    product_trade.append({"from_product": Product.QASHUSD, "mid_product": Product.QASHETH, "to_product": Product.ETHUSD, "base_fund": 15})
+    product_trade.append({"from_product": Product.ETHUSD, "mid_product": Product.ETHBTC, "to_product": Product.BTCUSD, "base_fund": 15})
     product_trade.append({"from_product": Product.QASHJPY, "mid_product": Product.QASHBTC, "to_product": Product.BTCJPY, "base_fund": 5000})
     product_trade.append({"from_product": Product.ETHJPY, "mid_product": Product.ETHBTC, "to_product": Product.BTCJPY, "base_fund": 5000})
     s.enter(2, 1, strategy.run, argument=(product_trade, trade_info, ))
