b268337cda1410fcab9eaefdc7bf268a724ade86
yy-bird <guoqiang2648@gmail.com>
2018-01-17 15:50:59 +0800

    adjust to more agrassive trading

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ client = Quoinex(Account())
 strategy = Strategy()
 
 def scheduler(s, products):
-    s.enter(3, 1, strategy.run, argument=(products,))
+    s.enter(2, 1, strategy.run, argument=(products,))
     s.run()
 
 usd_balance = client.get_fiat_account_balance('USD')
@@ -20,6 +20,6 @@ eth_balance = client.get_crypto_account_balance('ETH')
 
 while 1:
     print('USD: {0} SGD: {1} QASH: {2} BTC: {3} ETH: {4}'.format(usd_balance, sgd_balance, qash_balance, btc_balance, eth_balance))
-    products = [{"from_product": Product.QASHUSD, "mid_product": Product.QASHBTC, "to_product": Product.BTCUSD, "base_fund":50}]#,
+    products = [{"from_product": Product.QASHUSD, "mid_product": Product.QASHBTC, "to_product": Product.BTCUSD, "base_fund": 100}]#,
                 # {"from_product": Product.QASHUSD, "mid_product": Product.QASHETH, "to_product": Product.ETHUSD, "base_fund": 15}]
     scheduler(s, products)
